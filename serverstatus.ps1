c95f91ba61d1e765df3c9ebd5d726c055337d56d
Git-North <72702125+Git-North@users.noreply.github.com>
2023-07-21 17:17:19 +0300

    working on it

---
+++ serverstatus.ps1
@@ -0,0 +1,9 @@
+
+$Server = Read-Host -Prompt 'Please Enter Your Server IP'
+
+curl -k -s https://api.mcsrvstat.us/2/$Server -O
+
+Get-Content .\$server | % {$_ -replace "§","" } | Out-File .\$server.server.json -Force
+Remove-Item .\$server -Force
+$server.server.json -replace '(?m)(?<=^([^"]|"[^"]*")*)//.*' -replace '(?ms)/\*.*?\*/'
+Get-Content -Raw  ".\$Server.server.json" | ConvertFrom-Json | Select-Object -Property online 
