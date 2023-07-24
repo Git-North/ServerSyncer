c7becc6fbcab8415af00a42911610fe704ccd189
Git-North <72702125+Git-North@users.noreply.github.com>
2023-07-24 17:56:41 +0300

    functions

--- serverstatus.ps1
+++ serverstatus.ps1
@@ -1,6 +1,9 @@
-$ip = Read-Host -Prompt 'Please Enter Your Server IP'
-curl -k -s https://api.mcsrvstat.us/2/$ip -O
+function new-ip {
+    ($ip = Read-Host -Prompt 'Please Enter Your Server IP')
+}
+curl -k -s https://api.mcsrvstat.us/2/$ip -O 
 
+new-ip
 Get-Content .\$ip | % {$_ -replace "§","" } | Out-File .\$ip.server.json -Force
 Remove-Item .\$ip -Force
 $ip.server.json -replace '(?m)(?<=^([^"]|"[^"]*")*)//.*' -replace '(?ms)/\*.*?\*/'
