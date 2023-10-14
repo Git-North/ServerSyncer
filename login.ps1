b65d91934b8d9c672b818415e43050808fe07224
Git-North <72702125+Git-North@users.noreply.github.com>
2023-10-15 01:00:13 +0300

    tests

--- login.ps1
+++ login.ps1
@@ -1,4 +1,23 @@
-echo "Please Enter Your Email Address" 
-Read-Host -AsSecureString | ConvertFrom-SecureString | Out-File -FilePath "./mail.encrypted"
-echo "Please Enter Your Password"
-Read-Host -AsSecureString | ConvertFrom-SecureString | Out-File -FilePath "./pass.encrypted"
+$MegaCreds = Get-Credential
+$credpath = "./creds/mega.encrypted"
+$MegaCreds | Export-Clixml -Path $credpath
+$credpath = Import-Clixml -Path $credpath
+
+
+
+
+
+
+
+
+#$pw = Read-Host -AsSecureString
+#[pscredential]::new('user',$pw).GetNetworkCredential().Password
+#$pw | ConvertFrom-SecureString | Out-File -FilePath "./pass.encrypted"
+#$encStr = Get-Content "./pass.encrypted"
+#$encStr | ConvertFrom-SecureString 
+
+
+#echo "Please Enter Your Email Address" 
+# Read-Host -AsSecureString | ConvertFrom-SecureString | Out-File -FilePath "./mail.encrypted"
+# echo "Please Enter Your Password"
+# Read-Host -AsSecureString | ConvertFrom-SecureString | Out-File -FilePath "./pass.encrypted"
