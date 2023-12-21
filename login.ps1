4841745c6b744fec11e4c0f9f65d18ad966fc52f
Git-North <72702125+Git-North@users.noreply.github.com>
2023-12-21 20:19:45 +0300

    Update login.ps1

--- login.ps1
+++ login.ps1
@@ -1,14 +1,12 @@
-$date = (get-date).ToUniversalTime().ToString("dd-MM-yyyy,HH.mm.s")
+$fileformat = (get-date).ToUniversalTime().ToString(";d-MM-yyyy,HH.mm.ss")
 (test-path creds) ? $null : (mkdir creds)
 $MegaCreds = Get-Credential
 $crednew = "./creds/creds.new"
 $MegaCreds | Export-Clixml -Path $crednew
 $creds = Import-Clixml -Path $crednew
-Rename-Item "$crednew" $creds.UserName
-#add $date
-
+$xmlname = $creds.username + $fileformat 
+Rename-Item "$crednew" "$xmlname.xml"
 #$creds.password | ConvertFrom-SecureString
-
 $Ptr = [System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($creds.Password)
 $result = [System.Runtime.InteropServices.Marshal]::PtrToStringUni($Ptr)
 [System.Runtime.InteropServices.Marshal]::ZeroFreeCoTaskMemUnicode($Ptr)
