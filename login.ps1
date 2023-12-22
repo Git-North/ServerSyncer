785d445aeb7d26fac48a441bd3bdd808716005fa
Git-North <72702125+Git-North@users.noreply.github.com>
2023-12-23 00:46:34 +0300

    Update login.ps1

--- login.ps1
+++ login.ps1
@@ -14,9 +14,32 @@ foreach ($service in $services) {
     }
 
     $fileObjects | Format-Table -AutoSize
+
+    $selectedFile = $null
+
+    while ($selectedFile -eq $null) {
+        $selectedFileName = Read-Host "Enter the username (file name without extension) to process (press Enter to end)"
+        
+        if (-not $selectedFileName) {
+            break  # Exit the loop if the user presses Enter without typing anything
+        }
+
+        $selectedFile = $files | Where-Object { $_.BaseName -eq $selectedFileName }
+
+        if ($selectedFile -eq $null) {
+            Write-Host "File not found. Please enter a valid username."
+        }
+    }
+
+    if ($selectedFile -ne $null) {
+        $importedData = Import-Clixml -Path $selectedFile.FullName
+
+        # Your existing code block using $importedData
+        $Ptr = [System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($importedData.Password)
+        $result = [System.Runtime.InteropServices.Marshal]::PtrToStringUni($Ptr)
+        [System.Runtime.InteropServices.Marshal]::ZeroFreeCoTaskMemUnicode($Ptr)
+
+        # Process $result or perform any additional actions
+        Write-Host "Password result: $result"
+    }
 }
-# 
-# $Ptr = [System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($creds.Password)
-# $result = [System.Runtime.InteropServices.Marshal]::PtrToStringUni($Ptr)
-# [System.Runtime.InteropServices.Marshal]::ZeroFreeCoTaskMemUnicode($Ptr)
-# #$result
